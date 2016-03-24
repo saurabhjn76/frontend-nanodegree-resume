c88839b63b891098f0497ffc8265e6f291c3af6a
saurabhjn76 <saurabhjn76@gmail.com>
2016-03-24 22:29:42 +0530

    Update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,27 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var name ="Saurabh Jain";
+ var role="Programmer , Sports enthusiast"
+
+ var formattedName=HTMLheaderName.replace("%data%",name);
+ $("#header").prepend(formattedName);
+  var formattedRole=HTMLheaderRole.replace("%data%",role);
+ $("#header").prepend(formattedRole);
+
+ var bio={
+ 	"name":name,
+ 	"role":role,
+ 	"contacts":{
+ 		"mobile":"+91-9868043072",
+ 		"email":"saurabhjn76@gmail.com",
+ 		"github":"saurabhjn76",
+ 		"twitter":"@saurabhjn76",
+ 		"location":"noida",
+ 	},
+ 	"welcomeMessage":"lorem ipsum blah blah.....",
+ 	"skills":["multitasking","foody","Sportsperson","coder"],
+ 	"bioPic":"images/fry.jpg"
+
+ };
+ $("#main").preappend(bio);
